6d65ee00f1c0045d553f35b1864714d60464c13e
Chaitanya <chaitanya4974@gmail.com>
2024-03-01 12:30:43 -0800

    added first

---
+++ Downloads/untitled/src/main/java/com/learning/Sorting.java
@@ -0,0 +1,12 @@
+package com.learning;
+
+import java.util.Arrays;
+
+public class Sorting {
+    public static void main(String[] args) {
+        int array[]={5,7,8,3};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+}
