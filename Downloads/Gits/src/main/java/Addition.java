c826994011852460152b9337eeca0bc7d908f831
Chaitanya <chaitanya4974@gmail.com>
2024-03-05 23:14:03 -0800

    addition number

---
+++ Downloads/Gits/src/main/java/Addition.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number here");
+        int a= sc.nextInt();
+        System.out.println("Enter second number here");
+        int b= sc.nextInt();
+        System.out.println("sum will is"+" "+(a+b));
+    }
+}
